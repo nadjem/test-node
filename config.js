30b572adbf043424cb96a76aee1d4b1f255258d6
nadjem <31990453+nadjem@users.noreply.github.com>
2018-03-28 17:44:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 process.env.port = 3000;
 
-process.env.dbHost = "ip_adress";
+process.env.dbHost = "*ip_adress*";
 process.env.dbName = "testeur_db";
 process.env.dbUser = "testeur";
 process.env.dbPassword = "password";
 process.env.tokenSecret = "tokenSecret";
-process.env.secretToken = 'secretToken';
+process.env.secret = 'secret';
